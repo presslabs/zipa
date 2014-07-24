964d4367cdb33e52ec5395b2a6502b442df7a16f
Calin Don <calin.don@gmail.com>
2014-07-24 13:04:26 +0200

    removed development deps from setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,6 @@
 from setuptools import setup
 
-requires_list = [
-    'coverage==3.7.1',
-    'mock==1.0.1',
-    'nose==1.3.0',
-    'requests==1.2.3',
-]
-
-setup(name='papi',
+setup(name='zipa',
       version='1.00',
       platforms='any',
       description='Universal REST API client',
@@ -16,7 +9,7 @@ setup(name='papi',
       url='https://github.com/vtemian/papi',
       packages=['papi'],
       include_package_data=True,
-      install_requires=requires_list,
+      install_requires=['requests'],
       classifiers=[
           'Programming Language :: Python :: 2.7',
       ])
