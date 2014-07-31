59b8437f1f706ed0ccacb71b0830adbbfc89e6ef
Calin Don <calin.don@gmail.com>
2014-07-31 21:47:56 +0300

    use mocking per test case

--- tests/test_resource.py
+++ tests/test_resource.py
@@ -1,19 +1,20 @@
-import mock
 import pytest
 
 from zipa import api_github_com as gh
 from zipa import Resource
+from mock import MagicMock
+
+mock_Resource = MagicMock(Resource)
 
 
-@mock.patch('zipa.Resource')
-def test_resource_trait_names(mock_Resource):
+def test_resource_trait_names():
     assert mock_Resource.call_count == 0
     with pytest.raises(AttributeError):
         gh.trait_names
     assert mock_Resource.call_count == 0
 
-@mock.patch('zipa.Resource')
-def test_resource__getAttributeNames(mock_Resource):
+
+def test_resource__getAttributeNames():
     assert mock_Resource.call_count == 0
     with pytest.raises(AttributeError):
         gh._getAttributeNames
