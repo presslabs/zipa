a8a3f77f2628c9ec9621834f439c4f85d5c9e156
Vlad Temian <vladtemian@gmail.com>
2017-05-17 17:08:05 +0300

    Move httpretty fixtures to pytest fixture

---
+++ tests/fixtures.py
@@ -0,0 +1,74 @@
+import pytest
+import httpretty
+
+
+@pytest.fixture
+def pretty_api():
+
+    httpretty.register_uri(httpretty.GET, 'http://api.test.com/item/a',
+                           status=200,
+                           content_type='application/json',
+                           body=u'{"name": "a"}')
+    httpretty.register_uri(httpretty.GET, 'http://api.test.com/list',
+                           status=200,
+                           content_type='application/json',
+                           body=u'[{"item1": "name1"},{"item2": "name2"}]',
+                           adding_headers={
+                               'Link': '<http://api.test.com/list/2>; '
+                                       'rel="next"',
+                           })
+    httpretty.register_uri(httpretty.GET, 'http://api.test.com/list/2',
+                           status=200,
+                           content_type='application/json',
+                           body=u'[{"item3": "name3"},{"item4": "name4"}]',
+                           adding_headers={
+                               'Link': '<http://api.test.com/list/3>; '
+                                       'rel="next"',
+                           })
+    httpretty.register_uri(httpretty.GET, 'http://api.test.com/list/3',
+                           status=200,
+                           content_type='application/json',
+                           body=u'[{"item5": "name5"}]')
+
+    httpretty.register_uri(httpretty.GET, 'http://api.test.com/list/first',
+                           status=200,
+                           content_type='application/json',
+                           body=u'[{"item1": "name1"},{"item2": "name2"}]',
+                           adding_headers={
+                               'Link': '<http://api.test.com/list/error>; '
+                                       'rel="next"',
+                           })
+    httpretty.register_uri(httpretty.GET, 'http://api.test.com/list/error',
+                           status=400,
+                           content_type='application/json',
+                           body=u'{"detail":"error"}')
+
+
+    httpretty.register_uri(httpretty.GET, 'http://api.test.com/a',
+                           status=200,
+                           content_type='application/json',
+                           body=u'{"name": "a"}')
+    httpretty.register_uri(httpretty.POST, 'http://api.test.com/a',
+                           status=200,
+                           content_type='application/json',
+                           body=u'{"name": "a"}')
+    httpretty.register_uri(httpretty.PUT, 'http://api.test.com/a',
+                           status=200,
+                           content_type='application/json',
+                           body=u'{"name": "a"}')
+    httpretty.register_uri(httpretty.PATCH, 'http://api.test.com/a',
+                           status=200,
+                           content_type='application/json',
+                           body=u'{"name": "a"}')
+    httpretty.register_uri(httpretty.DELETE, 'http://api.test.com/a',
+                           status=200,
+                           content_type='application/json',
+                           body=u'{"name": "a"}')
+    httpretty.register_uri(httpretty.GET, 'http://api.test.com/b/c/d',
+                           status=200,
+                           content_type='application/json',
+                           body=u'{"name": "a"}')
+    httpretty.register_uri(httpretty.GET, 'http://api.test.com/s/resource',
+                           status=200,
+                           content_type='application/json',
+                           body=u'{"name": "a"}')
--- tests/test_http_methods.py
+++ tests/test_http_methods.py
@@ -2,43 +2,12 @@ import pytest
 import httpretty
 
 from zipa import api_test_com as t
-
-
-def pretty_api():
-    httpretty.register_uri(httpretty.GET, 'http://api.test.com/a',
-                           status=200,
-                           content_type='application/json',
-                           body=u'{"name": "a"}')
-    httpretty.register_uri(httpretty.POST, 'http://api.test.com/a',
-                           status=200,
-                           content_type='application/json',
-                           body=u'{"name": "a"}')
-    httpretty.register_uri(httpretty.PUT, 'http://api.test.com/a',
-                           status=200,
-                           content_type='application/json',
-                           body=u'{"name": "a"}')
-    httpretty.register_uri(httpretty.PATCH, 'http://api.test.com/a',
-                           status=200,
-                           content_type='application/json',
-                           body=u'{"name": "a"}')
-    httpretty.register_uri(httpretty.DELETE, 'http://api.test.com/a',
-                           status=200,
-                           content_type='application/json',
-                           body=u'{"name": "a"}')
-    httpretty.register_uri(httpretty.GET, 'http://api.test.com/b/c/d',
-                           status=200,
-                           content_type='application/json',
-                           body=u'{"name": "a"}')
-    httpretty.register_uri(httpretty.GET, 'http://api.test.com/s/resource',
-                           status=200,
-                           content_type='application/json',
-                           body=u'{"name": "a"}')
+from .fixtures import pretty_api
 
 
 @pytest.mark.httpretty
 @pytest.mark.parametrize('method', ['__call__', 'post', 'put', 'delete', 'patch'])
-def test_http_methods_over_simple_enpoint(method):
-    pretty_api()
+def test_http_methods_over_simple_enpoint(method, pretty_api):
     t.config.secure = False
     t.config.headers = {
         'x-custom-header': 'custom-value'
@@ -49,8 +18,7 @@ def test_http_methods_over_simple_enpoint(method):
 
 
 @pytest.mark.httpretty
-def test_custom_url_as_resource():
-    pretty_api()
+def test_custom_url_as_resource(pretty_api):
     resource_url = "http://api.test.com/b/c/d"
     assert t.resource(resource_url)().name == "a"
     assert t.s.resource().name == "a"
