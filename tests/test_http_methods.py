7d044ec0e50f2070aa71e286f2f259268d238c93
Vlad Temian <vladtemian@gmail.com>
2017-05-17 17:01:32 +0300

    Add custom header for delete method as well

--- tests/test_http_methods.py
+++ tests/test_http_methods.py
@@ -1,3 +1,4 @@
+import pytest
 import httpretty
 
 from zipa import api_test_com as t
@@ -34,21 +35,22 @@ def pretty_api():
                            body=u'{"name": "a"}')
 
 
+@pytest.mark.httpretty
+@pytest.mark.parametrize('method', ['__call__', 'post', 'put', 'delete', 'patch'])
+def test_http_methods_over_simple_enpoint(method):
+    pretty_api()
+    t.config.secure = False
+    t.config.headers = {
+        'x-custom-header': 'custom-value'
+    }
 
-class TestResourceIter(object):
+    assert getattr(t.a, method)().name == "a"
+    assert httpretty.last_request().headers['x-custom-header'] == 'custom-value'
 
-    @httpretty.activate
-    def test_http_methods_over_simple_enpoint(self):
-        pretty_api()
-        t.config.secure = False
 
-        assert t.a().name == "a"
-        assert t.a.post().name == "a"
-        assert t.a.put().name == "a"
-        assert t.a.delete().name == "a"
-        assert t.a.patch().name == "a"
-
-        resource_url = "http://api.test.com/b/c/d"
-        assert t.resource(resource_url)().name == "a"
-
-        assert t.s.resource().name == "a"
+@pytest.mark.httpretty
+def test_custom_url_as_resource():
+    pretty_api()
+    resource_url = "http://api.test.com/b/c/d"
+    assert t.resource(resource_url)().name == "a"
+    assert t.s.resource().name == "a"
