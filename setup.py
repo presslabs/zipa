24b9c4463c2e04d42832679d3576b996ea57a4a0
Vlad Temian <vladtemian@gmail.com>
2017-06-20 16:05:38 +0300

    Bump version to 0.3.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='zipa',
-      version="0.3.3",
+      version="0.3.4",
       platforms='any',
       description='General purpose REST API client',
       author='Presslabs SRL',
